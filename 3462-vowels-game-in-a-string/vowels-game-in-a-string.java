dabd3cd1c3412a23881d4819846bb87c03648319
Naman2401naman <153358667+Naman2401naman@users.noreply.github.com>
2025-09-12 20:32:23 +0530

    Time: 2 ms (100.00%) | Memory: 45.6 MB (66.93%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean doesAliceWin(String s) {
-        int count=0;
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(ch=='e' || ch=='a' || ch=='i' || ch=='o' || ch=='u'){
-                return true;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case 'a','e','i','o','u':
+                    return true;
             }
-        } 
-        return false; 
+        }
+
+        return false;
     }
 }
