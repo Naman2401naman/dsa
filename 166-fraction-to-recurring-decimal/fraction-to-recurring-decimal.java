e8336d5c6c0c12603f1e920bc0dde1eed93f9571
Naman2401naman <153358667+Naman2401naman@users.noreply.github.com>
2025-09-24 08:01:20 +0530

    Time: 1 ms (99.80%) | Memory: 41.3 MB (28.64%) - LeetSync

---
+++ 166-fraction-to-recurring-decimal/fraction-to-recurring-decimal.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+       if(numerator==0){
+        return "0";
+       }
+       StringBuilder str=new StringBuilder();
+       if(numerator<0 ^ denominator<0){
+            str.append("-");
+       }
+
+       long num=Math.abs((long)numerator);
+       long den=Math.abs((long)denominator);
+       str.append(num/den);
+       num=num%den;
+       if(num==0){
+        return str.toString();
+       }
+
+       str.append(".");
+        Map<Long, Integer> map = new HashMap<>();
+        while(num!=0){
+            if(map.containsKey(num)){
+                int index=map.get(num);
+                str.insert(index,"(");
+                str.append(")");
+                break;
+            }
+            map.put(num,str.length());
+            num*=10;
+            str.append(num/den);
+            num=num%den;
+
+        }
+        return str.toString();
+    }
+}
