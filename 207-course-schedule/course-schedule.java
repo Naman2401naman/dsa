751bcf43f43dbe5f2de502754f0118bec4d0426a
Naman2401naman <153358667+Naman2401naman@users.noreply.github.com>
2025-10-07 08:26:49 +0530

    Time: 31 ms (5.01%) | Memory: 44.8 MB (94.69%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indeg=new int[numCourses];
+        int[] ans=new int[numCourses];
+        for(int i=0;i<prerequisites.length;i++){
+            int idx=prerequisites[i][0];
+            indeg[idx]++;
+        }
+        Queue<Integer> queue=new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(indeg[i]==0){
+                queue.add(i);
+            }
+        }
+        int cnt=0;
+        while(!queue.isEmpty()){
+            int finder=queue.poll();
+            ans[cnt]=finder;
+            for(int i=0;i<prerequisites.length;i++){
+                if(prerequisites[i][1]==finder){
+                    int idx=prerequisites[i][0];
+                    indeg[idx]--;
+                    if(indeg[idx]==0){
+                        queue.add(idx);
+                    }
+                }
+            }
+            cnt++;
+        }
+        if(cnt==numCourses) return true;
+        else return false;
+    }
+}
