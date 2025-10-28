38a3b3f1167a3726e7f13c1282d89e4ac262c560
Naman2401naman <153358667+Naman2401naman@users.noreply.github.com>
2025-10-28 18:15:13 +0530

    Time: 3 ms (84.14%) | Memory: 44.9 MB (47.94%) - LeetSync

---
+++ 764-n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.java
@@ -0,0 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+              List<List<Integer>> result = new ArrayList<>();
+        
+        if (root == null) {
+            return result;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                Node current = queue.poll(); // Get the front node
+                currentLevel.add(current.val);   // Add its value
+                for(Node child:current.children){
+                    queue.offer(child);
+                }
+                
+            }
+
+            result.add(currentLevel); // Add this level to result
+        }
+
+        return result;
+    }
+}
