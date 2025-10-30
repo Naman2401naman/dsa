0503a996da7d9dcc70c6d893f57cb7182aa0453b
Naman2401naman <153358667+Naman2401naman@users.noreply.github.com>
2025-10-30 09:29:30 +0530

    Time: 1 ms (95.77%) | Memory: 44.3 MB (66.51%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,27 +15,26 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        HashMap<Integer,Integer> indexMap=new HashMap<>();
-        for(int i=0;i<inorder.length;i++){
-            indexMap.put(inorder[i],i);
-        }
+        HashMap<Integer,Integer> hash=new HashMap<>();
         int n=preorder.length-1;
         int m=inorder.length-1;
-        TreeNode root=construct(preorder,inorder,indexMap,0,n,0,m);
-        return root;
+
+        for(int i=0;i<inorder.length;i++){
+            hash.put(inorder[i],i);
+        }
+        return construct(preorder,inorder,hash,0,n,0,m);
+
     }
-    static TreeNode construct(int[] preorder, int[] inorder, HashMap<Integer,Integer> indexMap,int preIndst,int preIndend,int inIndst,int inIndend){
-        // base condition
-        if(preIndst>preIndend || inIndst>inIndend){
+    TreeNode construct(int[] preorder, int[] inorder,HashMap<Integer,Integer> hash,int preSt,int preN,int inSt,int inN){
+        if(preN<preSt){
             return null;
         }
-        int rootData=preorder[preIndst];
-        int rootIndex=indexMap.get(rootData);
-        TreeNode root=new TreeNode(rootData);
-        int leftree=rootIndex-inIndst;
-        int rightree=inIndend-rootIndex;
-        root.left=construct(preorder,inorder,indexMap,preIndst+1,preIndst+leftree,inIndst,rootIndex-1);
-        root.right=construct(preorder,inorder,indexMap,preIndst+leftree+1,preIndst+leftree+rightree,rootIndex+1,inIndend);
+
+        int val=preorder[preSt];
+        int Index=hash.get(val);
+        TreeNode root=new TreeNode(val);
+        root.left=construct(preorder,inorder,hash,preSt+1,preSt+(Index-inSt),inSt,Index-1);
+        root.right=construct(preorder,inorder,hash,preSt+(Index-inSt)+1,preN,Index+1,inN);
         return root;
     }
 }
