0517633b3f7677a28dbf2f7eeff22af55c052e97
Naman2401naman <153358667+Naman2401naman@users.noreply.github.com>
2025-12-11 15:07:46 +0530

    Time: 424 ms (15.79%) | Memory: 282.9 MB (8.42%) - LeetSync

--- 3819-count-covered-buildings/count-covered-buildings.java
+++ 3819-count-covered-buildings/count-covered-buildings.java
@@ -6,7 +6,6 @@ class Solution {
         // Map: y -> all x coordinates on that horizontal line
         HashMap<Integer, TreeSet<Integer>> mp_y = new HashMap<>();
 
-        // Fill maps
         for (int[] b : buildings) {
             int x = b[0];
             int y = b[1];
@@ -14,27 +13,20 @@ class Solution {
             mp_x.putIfAbsent(x, new TreeSet<>());
             mp_y.putIfAbsent(y, new TreeSet<>());
 
-            mp_x.get(x).add(y); // add y to x's vertical list
-            mp_y.get(y).add(x); // add x to y's horizontal list
+            mp_x.get(x).add(y);
+            mp_y.get(y).add(x);
         }
 
         int count = 0;
 
-        // Check every building
         for (int[] b : buildings) {
             int x = b[0];
             int y = b[1];
 
-            TreeSet<Integer> ys = mp_x.get(x); // all y for this x
-            TreeSet<Integer> xs = mp_y.get(y); // all x for this y
-
-            // If y is the smallest OR largest in its vertical list → boundary
+            TreeSet<Integer> ys = mp_x.get(x); 
+            TreeSet<Integer> xs = mp_y.get(y); 
             if (ys.first() == y || ys.last() == y) continue;
-
-            // If x is the smallest OR largest in its horizontal list → boundary
             if (xs.first() == x || xs.last() == x) continue;
-
-            // Otherwise it is surrounded from all 4 sides
             count++;
         }
 
