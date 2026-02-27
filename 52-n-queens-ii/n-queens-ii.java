3c1d1f2addc835f4ec7f085eca7c1ca9fd04e74f
Naman2401naman <153358667+Naman2401naman@users.noreply.github.com>
2026-02-27 11:32:03 +0530

    Time: 13 ms (5.65%) | Memory: 42.8 MB (16.31%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+class Solution {
+
+    List<List<String>> res = new ArrayList<>();
+    int N;
+
+    public int totalNQueens(int n) {
+
+        N = n;
+
+        List<List<String>> board = new ArrayList<>();
+
+        // create empty board with "."
+        for (int i = 0; i < n; i++) {
+            List<String> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                row.add(".");
+            }
+            board.add(row);
+        }
+
+        solve(board, 0);
+        return res.size();
+    }
+
+    void solve(List<List<String>> board, int row) {
+
+        if (row == N) {
+            // IMPORTANT: make deep copy
+            List<String> temp = new ArrayList<>();
+            for (List<String> r : board) {
+                temp.add(String.join("", r));
+            }
+            res.add(temp);
+            return;
+        }
+
+        for (int col = 0; col < N; col++) {
+
+            if (isValid(board, row, col)) {
+
+                board.get(row).set(col, "Q");
+                solve(board, row + 1);
+                board.get(row).set(col, ".");
+            }
+        }
+    }
+
+    boolean isValid(List<List<String>> board, int row, int col) {
+
+        // check column
+        for (int i = 0; i < row; i++) {
+            if (board.get(i).get(col).equals("Q"))
+                return false;
+        }
+
+        // check left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board.get(i).get(j).equals("Q"))
+                return false;
+        }
+
+        // check right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < N; i--, j++) {
+            if (board.get(i).get(j).equals("Q"))
+                return false;
+        }
+
+        return true;
+    }
+}
