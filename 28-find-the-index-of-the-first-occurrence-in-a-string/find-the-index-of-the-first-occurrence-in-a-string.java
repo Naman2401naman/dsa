7aa2e924ce37ae1894ab3582d860a9b215ffbc23
Naman2401naman <153358667+Naman2401naman@users.noreply.github.com>
2026-03-27 02:32:23 +0530

    Time: 4 ms (6.20%) | Memory: 43.1 MB (46.38%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,25 +1,48 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int hLen = haystack.length();
-        int nLen = needle.length();
-        int nIndex = 0;
-        for(int i=0; i<hLen; i++){
-            // as long as the characters are equal, increment needleIndex
-            if(haystack.charAt(i)==needle.charAt(nIndex)){
-                nIndex++;
+        int idx=kmp(needle,haystack);
+        return idx;
+    }
+    int kmp(String needle,String haystack){
+        int[] arr=build(needle);
+        int i=0;
+        int j=0;
+        while(i < haystack.length()){
+            if(haystack.charAt(i)==needle.charAt(j)){
+                i++;
+                j++;
             }
-            else{
-                // start from the next index of previous start index
-                i=i-nIndex;
-                // needle should start from index 0
-                nIndex=0;
-            }
-            // check if needleIndex reached needle length
-            if(nIndex==nLen){
-                // return the first index
-                return i-nLen+1;
+            if(j==needle.length()) return i-j;
+
+            else if(i<haystack.length() && haystack.charAt(i)!=needle.charAt(j)){
+                if(j!=0){
+                    j=arr[j-1];
+                }else{
+                    i++;
+                }
             }
         }
         return -1;
+    }
+
+    int[] build(String needle ){
+        int[] lps=new int[needle.length()];
+        int i=1;
+        int len=0;
+        while(i<needle.length()){
+            if(needle.charAt(i)==needle.charAt(len)){
+                len++;
+                lps[i]=len;
+                i++;
+            }else{
+                if(len!=0){
+                    len=lps[len-1];
+                }else{
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+        return lps;
     }
 }
