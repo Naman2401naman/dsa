4d208531d3c29f4a37d09f1b9b9e06652d707572
Naman2401naman <153358667+Naman2401naman@users.noreply.github.com>
2026-04-10 17:10:15 +0530

    Time: 3 ms (82.64%) | Memory: 45.1 MB (25.08%) - LeetSync

---
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int minimumDistance(int[] nums) {
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+
+        // Step 1: store indices
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], new ArrayList<>());
+            }
+            map.get(nums[i]).add(i);
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        // Step 2: check triples
+        for (List<Integer> list : map.values()) {
+            if (list.size() >= 3) {
+                for (int i = 0; i < list.size() - 2; i++) {
+                    int dist = 2 * (list.get(i + 2) - list.get(i));
+                    min = Math.min(min, dist);
+                }
+            }
+        }
+
+        return min == Integer.MAX_VALUE ? -1 : min;
+    }
+}
