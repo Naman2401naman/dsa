2f8dc2aa1ec2bfa834357bccfbaabc2015967a0f
Naman2401naman <153358667+Naman2401naman@users.noreply.github.com>
2026-05-11 11:13:58 +0530

    Time: 11 ms (7.38%) | Memory: 47 MB (15.69%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
@@ -1,17 +1,8 @@
 class Solution {
     public int[] separateDigits(int[] nums) {
-        List<Integer> ans=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            int n = nums[i];
-            List<Integer> temp=new ArrayList<>();
-            while(n>0){
-                int o=n%10;
-                n=n/10;
-                temp.add(o);
-            }
-            Collections.reverse(temp);
-            ans.addAll(temp);
-        }
-        return ans.stream().mapToInt(Integer::intValue).toArray();
+        return Arrays.stream(nums)
+                .mapToObj(String::valueOf).
+                flatMapToInt(str->str.chars().map(c->c-'0')).toArray();
+
     }
 }
