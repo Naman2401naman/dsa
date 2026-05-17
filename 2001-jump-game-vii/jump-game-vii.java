57f5a50bcbc1e58f3d05a6fcf9bcf49872e9a56a
Naman2401naman <153358667+Naman2401naman@users.noreply.github.com>
2026-05-17 11:14:13 +0530

    Time: 10 ms (61.48%) | Memory: 47.7 MB (79.19%) - LeetSync

--- 2001-jump-game-vii/jump-game-vii.java
+++ 2001-jump-game-vii/jump-game-vii.java
@@ -1,27 +1,24 @@
 class Solution {
     public boolean canReach(String s, int minJump, int maxJump) {
-        boolean[] dp=new boolean[s.length()];
-        if(s.charAt(s.length()-1)!='0'){
+        int l=s.length();
+        boolean[] dp=new boolean[l];
+        dp[0]=true;
+        if(s.charAt(l-1)=='1'){
             return false;
         }
-        dp[s.length()-1]=true;
-        for(int i=s.length()-2;i>=0;i--){
-            if(s.charAt(i)=='1'){
-                dp[i]=false;
-            }else{
-                if((i+minJump)>s.length()-1){
-                    dp[i]=false;
-                }else{
-                    for(int j=i+minJump;j<=Math.min(i+maxJump,s.length()-1);j++){
-                        if(dp[j]==true){
-                            dp[i]=true;
-                            break;
-                        }
-                    }
-                }
+        int reachable=0;
+        for(int i=1;i<l;i++){
+            if((i-minJump)>=0 && dp[i-minJump]){
+                reachable++;
+            }
+            if((i-maxJump-1)>=0 && dp[i-maxJump-1]){
+                reachable--;
             }
 
+            if(s.charAt(i)=='0' && reachable>0){
+                dp[i]=true;
+            }
         }
-        return dp[0];
+        return dp[l-1];
     }
 }
