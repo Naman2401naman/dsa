d807a7b9d3523e408d6837c86c7f347a8a15a2a6
Naman2401naman <153358667+Naman2401naman@users.noreply.github.com>
2026-07-12 13:39:49 +0530

    Time: 1080 ms (38.75%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,17 @@
+SELECT
+    s.student_id,
+    s.student_name,
+    sb.subject_name,
+    COUNT(e.subject_name) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects sb
+LEFT JOIN Examinations e on
+s.student_id=e.student_id
+and sb.subject_name=e.subject_name
+GROUP BY
+    s.student_id,
+    s.student_name,
+    sb.subject_name
+ORDER BY
+    s.student_id,
+    sb.subject_name;
