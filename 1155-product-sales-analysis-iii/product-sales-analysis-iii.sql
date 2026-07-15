4267e905dd42ad8bd544f01a4e3b84ff531c1909
Naman2401naman <153358667+Naman2401naman@users.noreply.github.com>
2026-07-15 12:05:01 +0530

    Time: 696 ms (75.58%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,12 @@
+SELECT
+    s.product_id,
+    s.year AS first_year,
+    s.quantity,
+    s.price
+FROM Sales s
+join (
+    select product_id,min(year) as first_year
+    from Sales
+    group by product_id
+)t
+on s.product_id=t.product_id and s.year=t.first_year;
