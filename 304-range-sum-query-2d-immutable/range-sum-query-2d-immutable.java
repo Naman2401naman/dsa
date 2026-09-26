0ab43f81f3e49d6ec7aee27c3091a49c5f6b1493
Naman2401naman <153358667+Naman2401naman@users.noreply.github.com>
2026-09-26 09:27:28 +0530

    Time: 108 ms (31.15%) | Memory: 144.8 MB (5.14%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,40 +1,36 @@
 class NumMatrix {
 
-    long[][] dp;
+    private long[][] prefix;
 
     public NumMatrix(int[][] matrix) {
 
         int n = matrix.length;
         int m = matrix[0].length;
 
-        dp = new long[n + 1][m + 1];
+        prefix = new long[n + 1][m + 1];
 
         for (int i = 1; i <= n; i++) {
 
             for (int j = 1; j <= m; j++) {
 
-                dp[i][j] =
-                    dp[i][j - 1] + matrix[i - 1][j - 1];
+                prefix[i][j] =
+                        matrix[i - 1][j - 1]
+                        + prefix[i - 1][j]
+                        + prefix[i][j - 1]
+                        - prefix[i - 1][j - 1];
             }
         }
     }
 
-    public int sumRegion(int row1, int col1, int row2, int col2) {
+    public long sumRegion(
+            int r1,
+            int c1,
+            int r2,
+            int c2) {
 
-        int minn = Math.min(row1, row2);
-        int maxx1 = Math.max(row1, row2);
-
-        int maxx = Math.max(col1, col2);
-        int minn1 = Math.min(col1, col2);
-
-        long ans = 0;
-
-        for (int i = minn; i <= maxx1; i++) {
-
-            ans += dp[i + 1][maxx + 1]
-                 - dp[i + 1][minn1];
-        }
-
-        return (int) ans;
+        return prefix[r2 + 1][c2 + 1]
+                - prefix[r1][c2 + 1]
+                - prefix[r2 + 1][c1]
+                + prefix[r1][c1];
     }
 }
